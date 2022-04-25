28153d7944d51a2f113b2878d37decee5876889b
eneshasimturkmen <enstrkmn@hotmail.com>
2022-04-25 21:16:45 +0300

    work 3 is done and about to be pushed to Github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
 
     //Doing awesome work 2 ! ====> github da commit etmediğimiz için satırın solunda mavi renkte bir yer olur.
 
+    //Doing awesome work3 !
+    //Awesome work 3 is done and ready to be committed
+    //and ready to be pushed to remote repository
+
 
 }
