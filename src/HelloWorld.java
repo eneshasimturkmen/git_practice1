39e0c1fdd4f9c64228165f96acbd5459f330d1b8
eneshasimturkmen <enstrkmn@hotmail.com>
2022-04-25 20:44:55 +0300

    work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,7 @@ public class HelloWorld {
 
     //Doing some work here(you can type anything you want)
 
-
-
+    //Doing awesome work 2 ! ====> github da commit etmediğimiz için satırın solunda mavi renkte bir yer olur.
 
 
 }
